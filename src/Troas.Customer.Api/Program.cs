cafeb2ccf659a6b4f2183e50545ff6fc98be88e9
Fredrick Troas Lutterodt <troasfl@gmail.com>
2024-11-07 23:09:32 -0700

    feat: configured required services

--- src/Troas.Customer.Api/Program.cs
+++ src/Troas.Customer.Api/Program.cs
@@ -1,34 +1,71 @@
-namespace Troas.Customer.Api;
+using Gelf.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Troas.Customer.Application.DbServices;
+using Troas.Customer.Infrastructure.Persistence;
 
-public class Program
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure Application Insights telemetry collection.
+builder.Services.AddApplicationInsightsTelemetry();
+
+// Configure GELF for Graylog
+builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddGelf(options =>
 {
-    public static void Main(string[] args)
+    options.AdditionalFields = new Dictionary<string, object>
     {
-        var builder = WebApplication.CreateBuilder(args);
+        {"facility", builder.Configuration.GetSection("Logging")["GELF:Facility"]},
+        {"Environment", builder.Configuration.GetSection("Logging")["GELF:Environment"]},
+        {"machine_name", Environment.MachineName}
+    };
+    options.Host = builder.Configuration.GetSection("Logging")["GELF:Host"];
+    options.LogSource = builder.Configuration.GetSection("Logging")["GELF:LogSource"];
+    options.Port = int.Parse(builder.Configuration.GetSection("Logging")["GELF:Port"]);
+}));
 
-        // Add services to the container.
+//Configure OpenTelemetry Tracing using Jaeger
+builder.Services.AddOpenTelemetry()
+    .WithTracing(b => b.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Environment.ApplicationName))
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter(opts => opts.Endpoint = new Uri("http://localhost:4317")));
 
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("PostgresConnection"),
+        b => b.MigrationsAssembly("Troas.Customer.Infrastructure")));
 
-        var app = builder.Build();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
-    }
+var app = builder.Build();
+        
+// Apply migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
 }
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseSwagger();
+app.UseSwaggerUI();
+        
+app.UseHttpsRedirection();
+app.UseAuthorization();
+        
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { } // Make Program class partial for testing purposes
